355e81ad82f998f2299983a953fb0142581e0b43
Artem <artembuinov05@gmail.com>
2023-01-19 04:26:40 +0300

    last task

---
+++ main-project/processing.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef PROCESSING_H
+#define PROCESSING_H
+
+#include "conf_prog.h"
+
+int process(conf_prog* array[], int size, int big);
+int processe(conf_prog* array[], int size);
+
+#endif
